174c98c26f8f9f2a8046c157fd5e0ee32ba793ed
princessbobsanta <princessbobsantaclaus@gmail.com>
2020-02-26 09:22:24 -1000

    better comments

--- test_csv_parsing.js
+++ test_csv_parsing.js
@@ -11,14 +11,17 @@ fs.readFile(dataFile, "utf8", (e, data) => {
     //split unix or windows style line endings, trim in case last line has line break
     //let rows = data.trim().split(/\r?\n/);
 
+    //handles windows or unix style rowend (think the spec only specified windows style, but better to be robust)
     let linebreak = /\r?\n/;
+    //rows delimited by linebreaks or end of string
     let rowend = `(?:${linebreak}|$)`;
     let fieldend = `(?:,|$)`;
     //. doesnt match linebreak characters
     let inclusiveDot = `(?:.|${linebreak})`;
 
-    
+    //standard field not wrapped in doublequotes cannot contain doublequotes, commas, or linebreak characters
     let standardField = /([^",\n\r]*?)/;
+    //non-capture group version
     let standardFieldNoCap = /(?:[^",\n\r]*?)/;
     
 
@@ -30,14 +33,17 @@ fs.readFile(dataFile, "utf8", (e, data) => {
     //any text followed by text including escape quotes
     //capture inside quotes
     let quotedField = `"(${textAny}${textWEscQuotes})"`;
+    //non-capture group version
     let quotedFieldNoCap = `"(?:${textWEscQuotes})"`;
 
-    //use captures from field defs since want to capture quoted fields inside quotes (create non-capture group here)
+    //field is one of the two field types, use captures from field defs since want to capture quoted fields inside quotes (create non-capture group here)
     let field = `(?:${standardField}|${quotedField})`;
     let fieldNoCap = `(?:${standardFieldNoCap}|${quotedFieldNoCap})`;
 
-    //any number of fields followed by commas followed by a field without a comma (don't capture group)
+    //use global defs to avoid kleene star capture issue
+    //row is any number of fields followed by commas followed by a field followed by a row delimiter (capture row without row delim)
     let rowDef = new RegExp(`((?:${fieldNoCap},)*${fieldNoCap})${rowend}`, "g");
+    //column is a field followed by a field delimeter, capture handled by field defs
     let columnDef = new RegExp(`${field}${fieldend}`, "g");
 
     //starred groups only capture last instance, ugh
